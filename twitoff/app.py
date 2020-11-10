d0494e8bf163d194fc99cf62ea6094544db9fd49
rick1310 <ricky.chance@gmail.com>
2020-11-09 18:08:35 -0600

    removed extraneous app.py and __init__.py files

---
+++ twitoff/app.py
@@ -0,0 +1,19 @@
+"""Main app routing for TwitOff"""
+
+from flask import Flask, render_template
+
+def create_app():
+    """Creating and configuring an instance of the flask application"""
+
+    app = Flask(__name__)
+
+    
+    # This is a decorator. Is used to add functionality within a function and return it. 
+    # @app.route sets the base URL for our app. 
+    @app.route('/')
+    def root():
+        return render_template('base.html', title="home")
+
+
+    return app
+
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,17 +1,17 @@
 """SQLalchemy models and utility functions for TwitOff"""
 
-from flask_sqlalchemy import SQLAlchemy
+from flask_
 
 DB = SQLAlchemy()
 
 class user(DB.Model):
     """Twitter User Table that will correspond to twets - SQLAlchemy syntax"""
     id = DB.Column(DB.BigInteger, primary_key =True) # id column (primary key)
-    name = DB.Column(DB.String, nullable=false )
+    name = DB.Column(DB.String,  )
 
     def __repr__(self):
         return "<User: {}>".format(self.name)
-        
+
 
 
 
