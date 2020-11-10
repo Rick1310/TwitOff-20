47aaae7a2e2492c866811ee7fe9ed47349f068d7
rick1310 <ricky.chance@gmail.com>
2020-11-10 11:34:02 -0600

    Created user tables and users

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,19 +1,30 @@
-"""Main app routing for TwitOff"""
-
 from flask import Flask, render_template
+from .models import DB, User, insert_example_users
 
+
+# creates application
 def create_app():
-    """Creating and configuring an instance of the flask application"""
-
+    """Creating and configuring an instance of the Flask application"""
     app = Flask(__name__)
 
-    
-    # This is a decorator. Is used to add functionality within a function and return it. 
-    # @app.route sets the base URL for our app. 
-    @app.route('/')
-    def root():
-        return render_template('base.html', title="home")
+    # database and app configurations
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    # initilizing database
+    DB.init_app(app)
+
+    # decorator listens for specific endpoint visits
+    @app.route('/')  # http://127.0.0.1:5000/
+    def root():
+        # we must create the database
+        DB.drop_all()
+        DB.create_all()
+        # avoiding error since we are dropping all values - no duplicate users
+        insert_example_users()
+        # renders base.html template and passes down title and users
+        return render_template('base.html', title="home", users=User.query.all())
 
     return app
+
 
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,21 +1,39 @@
 """SQLalchemy models and utility functions for TwitOff"""
 
-from flask_
+
+from flask_sqlalchemy import SQLAlchemy
+
 
 DB = SQLAlchemy()
 
-class user(DB.Model):
-    """Twitter User Table that will correspond to twets - SQLAlchemy syntax"""
-    id = DB.Column(DB.BigInteger, primary_key =True) # id column (primary key)
-    name = DB.Column(DB.String,  )
+
+# User table
+class User(DB.Model):
+    """Twitter User Table that will correspond to tweets - SQLAlchemy syntax"""
+    id = DB.Column(DB.BigInteger, primary_key=True)  # id column (primary key)
+    name = DB.Column(DB.String, nullable=False)  # name column
 
     def __repr__(self):
         return "<User: {}>".format(self.name)
 
 
+# Twitter table
+class Tweet(DB.Model):
+    """Tweet text data - associated with Users Table"""
+    id = DB.Column(DB.BigInteger, primary_key=True)  # id column (primary key)
+    text = DB.Column(DB.Unicode(300))
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey(
+        "user.id"), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return "<Tweet: {}>".format(self.text)
 
 
-
-
-
-
+def insert_example_users():
+    """We will get an error if we run this twice without dropping & creating"""
+    nick = User(id=1, name="nick")
+    elon = User(id=2, name="elonmusk")
+    DB.session.add(nick)
+    DB.session.add(elon)
+    DB.session.commit()
